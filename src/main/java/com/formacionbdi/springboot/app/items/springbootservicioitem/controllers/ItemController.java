3e024226b1c15e7605415f327e06fd7013046bc9
Kenyi De la cruz <kenyi_dlcp_93@outlook.com>
2022-07-06 00:00:55 -0500

    feat:UDEMY Predicate filter

--- src/main/java/com/formacionbdi/springboot/app/items/springbootservicioitem/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/items/springbootservicioitem/controllers/ItemController.java
@@ -4,9 +4,7 @@ import com.formacionbdi.springboot.app.items.springbootservicioitem.models.Item;
 import com.formacionbdi.springboot.app.items.springbootservicioitem.models.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +16,10 @@ public class ItemController {
   private ItemService itemService;
 
   @GetMapping("/listar")
-  public List<Item> listar(){
+  public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
+                           @RequestHeader(name = "token-request", required = false) String token){
+    System.out.println(nombre);
+    System.out.println(token);
     return itemService.findAll();
   }
 
