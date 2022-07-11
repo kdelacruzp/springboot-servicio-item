9e5d68d317981549560888a9070c36e5fe6994c8
Kenyi De la cruz <kenyi_dlcp_93@outlook.com>
2022-07-11 00:19:29 -0500

    feat:BCPUDEMY Configserver client configuration

--- src/main/java/com/formacionbdi/springboot/app/items/springbootservicioitem/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/items/springbootservicioitem/controllers/ItemController.java
@@ -1,15 +1,41 @@
 package com.formacionbdi.springboot.app.items.springbootservicioitem.controllers;
 
 import com.formacionbdi.springboot.app.items.springbootservicioitem.models.Item;
+import com.formacionbdi.springboot.app.items.springbootservicioitem.models.Producto;
 import com.formacionbdi.springboot.app.items.springbootservicioitem.models.service.ItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+
+@RefreshScope
 @RestController
 public class ItemController {
+
+  private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+  @Autowired
+  private Environment env;
+  @Autowired
+  private CircuitBreakerFactory cbFactory;
+
+  @Value("${configuracion.texto}")
+  private String texto;
 
   @Autowired
   @Qualifier("serviceFeign")
@@ -25,8 +51,65 @@ public class ItemController {
 
   @GetMapping("/ver/{id}/cantidad/{cantidad}")
   public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+    return cbFactory.create("items")
+        .run(() -> itemService.findById(id, cantidad),e -> metodoAlternativo(id, cantidad, e));
+  }
+
+  @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+  @GetMapping("/ver2/{id}/cantidad/{cantidad}")
+  public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
     return itemService.findById(id, cantidad);
   }
 
+  @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+  @TimeLimiter(name = "items")
+  @GetMapping("/ver3/{id}/cantidad/{cantidad}")
+  public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+    return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
+  }
+
+  public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+    logger.info(e.getMessage());
+
+    Item item = new Item();
+    Producto producto = new Producto();
+
+    item.setCantidad(cantidad);
+    producto.setId(id);
+    producto.setNombre("Camara Sony");
+    producto.setPrecio(500.00);
+    item.setProducto(producto);
+    return item;
+  }
+
+  public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+    logger.info(e.getMessage());
+
+    Item item = new Item();
+    Producto producto = new Producto();
+
+    item.setCantidad(cantidad);
+    producto.setId(id);
+    producto.setNombre("Camara Sony");
+    producto.setPrecio(500.00);
+    item.setProducto(producto);
+    return CompletableFuture.supplyAsync(() -> item);
+  }
+
+  @GetMapping("/obtener-config")
+  public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+    logger.info(texto);
+
+    Map<String, String> json = new HashMap<>();
+    json.put("texto", texto);
+    json.put("puerto", puerto);
+
+    if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+      json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+      json.put("autor.email", env.getProperty("configuracion.autor.email"));
+    }
+
+    return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+  }
 
 }
